a41254e80b086c12ec20383ab8174bf5a416e09a
gabito00 <gabrielm.montana@gmail.com>
2025-08-16 19:44:05 -0300

    Se agrego el ejercicio 9

---
+++ TP–Introduccion-a-Java/TP1E9/src/tp1e9/TP1E9.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp1e9;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class TP1E9 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingresa tu nombre: ");
+        String nombre = scanner.nextLine();
+        System.out.println("Hola, " + nombre);
+
+    }
+    
+}
