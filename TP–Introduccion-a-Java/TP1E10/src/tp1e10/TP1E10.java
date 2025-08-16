7008e271082e6d77cb5d25a634df6b6fe82aa154
gabito00 <gabrielm.montana@gmail.com>
2025-08-16 19:44:18 -0300

    Se agrego el ejercicio 10

---
+++ TP–Introduccion-a-Java/TP1E10/src/tp1e10/TP1E10.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp1e10;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class TP1E10 {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 2;
+        int resultado = a / b;
+        System.out.println("Resultado: " + resultado);
+    }
+    
+}
